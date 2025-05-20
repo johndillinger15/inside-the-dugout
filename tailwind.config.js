0729b1302dac152315c057989b9258f51580bcdf
johndillinger15 <dillinger.stefan@googlemail.com>
2025-05-20 21:55:08 +0200

    bump tailwind v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,31 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.njk',
-    './src/**/*.svg',
-    './src/assets/js/*.js'
-  ],
-  screens: {
-    sm: '576px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px'
-  },
+  content: ["./src/**/*.njk", "./src/**/*.svg", "./src/assets/js/*.js"],
   theme: {
     extend: {
-      typography: (theme) => ({
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      typography: {
         DEFAULT: {
           css: {
-            'blockquote': {
-              fontWeight: 'normal',
-              color: theme('colors.slate.600')
+            blockquote: {
+              fontWeight: "normal",
+              color: "#475569", // slate.600
             },
-            'blockquote p:first-of-type::before': {
-              content: ''
+            "blockquote p:first-of-type::before": {
+              content: "none",
             },
-            'blockquote p:last-of-type::after': {
-              content: ''
-            }
-          }
-        }
-      })
+            "blockquote p:last-of-type::after": {
+              content: "none",
+            },
+          },
+        },
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ]
+  plugins: [require("@tailwindcss/typography")],
 };

df01c11b0093704281de0a2426a0b579dc897b52
johndillinger15 <dillinger.stefan@googlemail.com>
2026-05-09 22:25:15 +0200

    new tags

---
+++ src/_data/formats.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "morning-briefing": "Morning Briefing",
+  storylines: "Storylines",
+  "giants-series-recap": "Giants Series Recap",
+  "series-spotlight": "Series Spotlight",
+  shortpost: "Shortpost",
+};
